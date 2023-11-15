81bc0df353e89ec236143c7ce20a0d039bafa097
sagar2190sharma <sagarsharma.ss329.ss46@gmail.com>
2023-11-15 07:27:45 +0530

    created landing or starting page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Landing() {
+
+  return (
+    <div className=' flex justify-center items-center h-screen'>
+        <div className=' bg-black px-10 py-10 rounded-xl '>
+            <div className="">
+                <h1 className='text-center text-white text-5xl mb-4 font-bold logo'>User Management</h1>
+            </div>
+            <div className=' flex justify-center mb-3'>
+                <Link to={'/'}
+                    className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
+                    User Details
+                </Link>
+            </div>
+            <div className=' flex justify-center mb-3'>
+                <Link to={'/'}
+                    className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
+                    Account Creation
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
